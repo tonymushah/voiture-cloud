ca4485bc70b647e7496e43971c17f0a525cc06a0
tonymushah <tonyramanantsalama@gmail.com>
2023-11-07 09:46:03 +0300

    Added `KilometrageInputFromVehicule`

--- src/main/java/mg/tonymushah/itu/cloud/voiture/types/inputs/KilometrageInputFromVehicule.java
+++ src/main/java/mg/tonymushah/itu/cloud/voiture/types/inputs/KilometrageInputFromVehicule.java
@@ -8,4 +8,37 @@ public class KilometrageInputFromVehicule {
     private int debut;
     private Optional<Integer> fin;
 
+    public OffsetDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(OffsetDateTime date) {
+        this.date = date;
+    }
+
+    public int getDebut() {
+        return debut;
+    }
+
+    public void setDebut(int debut) {
+        this.debut = debut;
+    }
+
+    public Optional<Integer> getFin() {
+        return fin;
+    }
+
+    public void setFin(Optional<Integer> fin) {
+        this.fin = fin;
+    }
+
+    public KilometrageInputFromVehicule() {
+    }
+
+    public KilometrageInputFromVehicule(OffsetDateTime date, int debut, Optional<Integer> fin) {
+        this.setDate(date);
+        this.setDebut(debut);
+        this.setFin(fin);
+    }
+
 }
